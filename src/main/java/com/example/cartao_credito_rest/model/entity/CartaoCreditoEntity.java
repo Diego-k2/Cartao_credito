e82b45117363f5789682fab190dc85b9c68cfb88
Diego-k2 <dSilva@netsoft.com>
2023-04-08 16:27:33 -0300

    Commit inicial

---
+++ src/main/java/com/example/cartao_credito_rest/model/entity/CartaoCreditoEntity.java
@@ -0,0 +1,85 @@
+package com.example.cartao_credito_rest.model.entity;
+
+import jakarta.persistence.*;
+
+import java.util.Date;
+import java.util.UUID;
+
+@Entity
+@Table(name = "tb_cartao_credito")
+public class CartaoCreditoEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID uuid;
+
+    @Column(name = "cl_nome_titular", nullable = false)
+    private String nomeTitular;
+
+    @Column(name = "cl_numero_cartao", nullable = false)
+    private String numeroCartao;
+
+    @Column(name = "cl_limite_cartao", nullable = false)
+    private double Limite;
+
+    @Column(name = "cl_dt_vencimento", nullable = false)
+    private Date validade;
+
+    @Column(name = "cl_valor_fatura", nullable = false)
+    private double valorFatura = 0.00;
+
+    @Column(name = "cl_isDesbloqueado", nullable = false)
+    private boolean isDesbloqueado = false;
+
+    @Column(name = "cl_senha", nullable = false)
+    private String senha;
+
+
+    public String getNomeTitular() {
+        return nomeTitular;
+    }
+
+    public void setNomeTitular(String nomeTitular) {
+        this.nomeTitular = nomeTitular;
+    }
+
+    public String getNumeroCartao() {
+        return numeroCartao;
+    }
+
+    public void setNumeroCartao(String numeroCartao) {
+        this.numeroCartao = numeroCartao;
+    }
+
+    public double getLimite() {
+        return Limite;
+    }
+
+    public void setLimite(double limite) {
+        Limite = limite;
+    }
+
+    public double getValorFatura() {
+        return valorFatura;
+    }
+
+    public void setValorFatura(double valorFatura) {
+        this.valorFatura = valorFatura;
+    }
+
+    public boolean isDesbloqueado() {
+        return isDesbloqueado;
+    }
+
+    public void setDesbloqueado(boolean desbloqueado) {
+        isDesbloqueado = desbloqueado;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+}
---
+++ src/test/java/com/example/cartao_credito_rest/CartaoCreditoRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cartao_credito_rest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CartaoCreditoRestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
